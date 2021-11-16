01ca90354674f2a1458ce54be925da4d05a1db66
LenPolina <polinalen1212@gmail.com>
2021-11-16 18:08:01 +0200

    - added first two years in equivalence classes

--- first/src/main/java/DayOfWeek/Year.java
+++ first/src/main/java/DayOfWeek/Year.java
@@ -7,15 +7,14 @@ public class Year {
 
     public String findNameOfDayOfTheWeek(int newYearDay, int day, int month) {
         int dayInYear = 0, dayOfTheWeek;
-        if (newYearDay - 1 == 0) dayOfTheWeek = 7;
+        if (newYearDay == 1) dayOfTheWeek = 7;
         else
             dayOfTheWeek = newYearDay - 1;
         for (int i = 0; i < month - 1; i++) {
             dayInYear += dayInMonth[i];
         }
         dayInYear += day;
-        /*if (dayInYear > 7)*/ dayOfTheWeek += dayInYear % 7;
-        /*else dayOfTheWeek += dayInYear;*/
+        dayOfTheWeek += dayInYear % 7;
         if (dayOfTheWeek > 7) dayOfTheWeek %= 7;
         return nameOfDayOfTheWeek[dayOfTheWeek];
     }
