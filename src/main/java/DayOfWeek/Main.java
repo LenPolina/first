7ded598cc85a6cf7d681b20f7708a73b8f2b7ef2
LenPolina <polinalen1212@gmail.com>
2021-11-16 10:07:26 +0200

    - setup environment.

--- src/main/java/DayOfWeek/Main.java
+++
@@ -1,9 +0,0 @@
-package DayOfWeek;
-
-public class Main {
-    public static void main(String[] args) {
-        Year year = new Year();
-        int newYearDay = 3, day = 17, month = 7;
-        System.out.println(year.findNameOfDayOfTheWeek(newYearDay,day,month));
-    }
-}
